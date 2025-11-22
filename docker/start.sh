212a031db9c7030bcc2fab9be1cb179468d00248
ellaarrogante18-ux <ellaarrogante18@gmail.com>
2025-11-22 12:53:11 +0800

    Add PHP-FPM TCP configuration to start.sh
    Added PHP-FPM configuration for TCP in start.sh.

--- docker/start.sh
+++ docker/start.sh
@@ -15,5 +15,22 @@ if [ "$RUN_MIGRATIONS_ON_START" = "true" ]; then
   php artisan migrate --force || true
 fi
 
+# Create PHP-FPM configuration for TCP
+echo '[global]
+daemonize = no
+error_log = /proc/self/fd/2
+
+[www]
+listen = 127.0.0.1:9000
+listen.allowed_clients = 127.0.0.1
+user = www-data
+group = www-data
+
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3' > /usr/local/etc/php-fpm.d/zz-render.conf
+
 # Start supervisord to run php-fpm + nginx
 exec /usr/bin/supervisord -n
